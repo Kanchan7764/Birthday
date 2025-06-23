dae84d9a2efc7ae86c093744e4b899c26a365aa9
Kanchan Gupta <kg235016@gmail.com>
2025-06-23 10:32:54 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import BirthdayApp from "./BirthdayApp";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   {/* <BirthdayApp /> */}
+  
    <App/>
   </StrictMode>,
 )
